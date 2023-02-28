44508f30783a66b02d2da398c5d3d1dafe250d81
andrewspies <andrew@oddlifedesigns.com>
2023-02-28 18:33:04 +0200

    clean up

--- src/server/api/UserApi.php
+++ src/server/api/UserApi.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once '../classes/UserManager.php';
+include('../classes/UserManager.php');
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
@@ -10,22 +10,21 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 
 $request = $_SERVER["REQUEST_METHOD"];
 
+$mananger = new UserManager();
+
 function handleRequest($request) {
   switch ($request) {
     case 'GET': 
-      $mananger = new UserManager();
       $data = $manager->getUsers();
       $this->sendResponse($data);
       break;
     case 'POST':
       $user = $request['body'];
-      $mananger = new UserManager();
       $data = $mananger->CreateUser($user);
       sendResponse($data);
       break;
     case 'DELETE':
       $user = $request['body'];
-      $mananger = new UserManager();
       $data = $mananger->deleteUser($user);
       sendResponse($data);
       break;
